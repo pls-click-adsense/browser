801759647c21044bb2bc25b60582670700c4f37f
pls-click-adsense <sousuzu@proton.me>
2026-04-25 15:48:00 +0900

    Update BrowserApp.swift

--- Sources/BrowserApp.swift
+++ Sources/BrowserApp.swift
@@ -1,15 +1,7 @@
 import SwiftUI
-import UIKit
+
 @main
 struct BrowserApp: App {
-    init() {
-        // ウィンドウの背景色をsystemBackgroundに設定
-        UIApplication.shared.connectedScenes
-            .compactMap { $0 as? UIWindowScene }
-            .flatMap { $0.windows }
-            .forEach { $0.backgroundColor = .systemBackground }
-    }
-
     var body: some Scene {
         WindowGroup {
             ContentView()
